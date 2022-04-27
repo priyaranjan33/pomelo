edfc524bd79435ca25b5d40a9a528b04154c6913
Priyaranjan <priyaranjanparida2014@gmail.com>
2022-04-27 21:41:32 +0530

    Updated Test Suite

--- src/test/java/com/test/shopping/Checkout.java
+++ src/test/java/com/test/shopping/Checkout.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
@@ -18,9 +17,9 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.test.constant.ProjectConstant;
-import com.test.driver.ChromeDriverConfig;
+import com.test.helper.ChromeDriverConfig;
 
-public class Checkout{
+public class Checkout {
 
 	static WebDriver driver;
 	static Wait<WebDriver> wait;
@@ -28,7 +27,7 @@ public class Checkout{
 	@BeforeTest
 	void setup() {
 		driver = ChromeDriverConfig.initialiseChromeDrive();
-		wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(60L))
+		wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(80L))
 				.pollingEvery(Duration.ofSeconds(8L)).ignoring(NoSuchElementException.class);
 
 	}
@@ -45,26 +44,22 @@ public class Checkout{
 	}
 
 	@Test
-	public void validateaQuantity() throws InterruptedException {
+	public void validateAdjustQuantity() throws InterruptedException {
 
 		// Add Product to Cart
-		WaitUntilElement(
-				"//li[@class='jsx-4161721015 header-section__ul-icon-search']//img[@src='https://pomelo-web-s3.pomelofashion.com/www/6.172.0/static/img/icons/system/search.svg']")
-						.click();
+		// Add Product to Cart
+		jsClick(WaitUntilElement(
+				"//li[@class='jsx-4161721015 header-section__ul-icon-search']//img[@src='https://pomelo-web-s3.pomelofashion.com/www/6.172.0/static/img/icons/system/search.svg']"));
+		jsClick(WaitUntilElement("//*[@id='header-search-form']"));
 
-		WebElement element = WaitUntilElement("//*[@id='header-search-form']");
-		JavascriptExecutor executor = (JavascriptExecutor) driver;
-		executor.executeScript("arguments[0].click();", element);
+		jsClick(WaitUntilElement("//input[@placeholder = 'What are you looking for today?...']"));
 
-		wait.until(ExpectedConditions
-				.elementToBeClickable(By.xpath("//input[@placeholder = 'What are you looking for today?...']")))
-				.click();
 		WaitUntilElement("//input[@class='jsx-1695452889 body2 pml-input__input']").sendKeys("56319");
 		WaitUntilElement("//input[@class='jsx-1695452889 body2 pml-input__input']").sendKeys(Keys.ENTER);
 
 		// Product View
-		WaitUntilElement("//div[@class='product-image__hover']//img[@alt='Short Sleeve Mandarin Collar Dress - Pink']")
-				.click();
+		jsClick(WaitUntilElement(
+				"//div[@class='product-image__hover']//img[@alt='Short Sleeve Mandarin Collar Dress - Pink']"));
 
 		WaitUntilElement("//button[.='L']").click();
 		WaitUntilElement("//span[.='Add To Bag']").click();
@@ -96,6 +91,7 @@ public class Checkout{
 		jsClick(WaitUntilElement("//span[.='Add To Bag']"));
 
 		jsClick(WaitUntilElement("//span[.='View My Shopping Bag']"));
+
 		// Adjust size of product
 		Select size = new Select(WaitUntilElement(
 				"//div[@class='jsx-958784943 pml-input-layout__container full-width cart-item-info__size']//select[@class='jsx-2192573192 pml-dropdown__select subtitle2 full-width']"));
@@ -106,26 +102,21 @@ public class Checkout{
 	@Test
 	public void validateDelete() throws InterruptedException {
 		driver.navigate().to(ProjectConstant.BASE_URL);
-		JavascriptExecutor executor = (JavascriptExecutor) driver;
 
-		WebElement elementSearch = WaitUntilElement(
-				"//li[@class='jsx-4161721015 header-section__ul-icon-search']//img[@src='https://pomelo-web-s3.pomelofashion.com/www/6.172.0/static/img/icons/system/search.svg']");
-		executor.executeScript("arguments[0].click();", elementSearch);
-		WebElement element2 = WaitUntilElement("//*[@id='header-search-form']");
+		// Add Product to Cart
+		jsClick(WaitUntilElement(
+				"//li[@class='jsx-4161721015 header-section__ul-icon-search']//img[@src='https://pomelo-web-s3.pomelofashion.com/www/6.172.0/static/img/icons/system/search.svg']"));
+		jsClick(WaitUntilElement("//*[@id='header-search-form']"));
 
-		executor.executeScript("arguments[0].click();", element2);
+		jsClick(WaitUntilElement("//input[@placeholder = 'What are you looking for today?...']"));
 
-		WebElement element3 = WaitUntilElement("//input[@placeholder = 'What are you looking for today?...']");
-
-		executor.executeScript("arguments[0].click();", element3);
 		WaitUntilElement("//input[@class='jsx-1695452889 body2 pml-input__input']").sendKeys("56319");
 		WaitUntilElement("//input[@class='jsx-1695452889 body2 pml-input__input']").sendKeys(Keys.ENTER);
 
 		// Product View
-		WebElement element4 = WaitUntilElement(
-				"//div[@class='product-image__hover']//img[@alt='Short Sleeve Mandarin Collar Dress - Pink']");
+		jsClick(WaitUntilElement(
+				"//div[@class='product-image__hover']//img[@alt='Short Sleeve Mandarin Collar Dress - Pink']"));
 
-		executor.executeScript("arguments[0].click();", element4);
 		WaitUntilElement("//button[.='L']").click();
 		WaitUntilElement("//span[.='Add To Bag']").click();
 
@@ -141,15 +132,18 @@ public class Checkout{
 		driver.navigate().to(ProjectConstant.BASE_URL);
 
 		// Add Product to Cart
-		WaitUntilElement(
-				"//li[@class='jsx-4161721015 header-section__ul-icon-search']//img[@src='https://pomelo-web-s3.pomelofashion.com/www/6.172.0/static/img/icons/system/search.svg']")
-						.click();
+		jsClick(WaitUntilElement(
+				"//li[@class='jsx-4161721015 header-section__ul-icon-search']//img[@src='https://pomelo-web-s3.pomelofashion.com/www/6.172.0/static/img/icons/system/search.svg']"));
+		jsClick(WaitUntilElement("//*[@id='header-search-form']"));
+
+		jsClick(WaitUntilElement("//input[@placeholder = 'What are you looking for today?...']"));
+
 		WaitUntilElement("//input[@class='jsx-1695452889 body2 pml-input__input']").sendKeys("56319");
 		WaitUntilElement("//input[@class='jsx-1695452889 body2 pml-input__input']").sendKeys(Keys.ENTER);
 
 		// Product View
-		WaitUntilElement("//div[@class='product-image__hover']//img[@alt='Short Sleeve Mandarin Collar Dress - Pink']")
-				.click();
+		jsClick(WaitUntilElement(
+				"//div[@class='product-image__hover']//img[@alt='Short Sleeve Mandarin Collar Dress - Pink']"));
 
 		WaitUntilElement("//button[.='L']").click();
 		WaitUntilElement("//span[.='Add To Bag']").click();
@@ -167,15 +161,18 @@ public class Checkout{
 		driver.navigate().to(ProjectConstant.BASE_URL);
 
 		// Add Product to Cart
-		WaitUntilElement(
-				"//li[@class='jsx-4161721015 header-section__ul-icon-search']//img[@src='https://pomelo-web-s3.pomelofashion.com/www/6.172.0/static/img/icons/system/search.svg']")
-						.click();
+		jsClick(WaitUntilElement(
+				"//li[@class='jsx-4161721015 header-section__ul-icon-search']//img[@src='https://pomelo-web-s3.pomelofashion.com/www/6.172.0/static/img/icons/system/search.svg']"));
+		jsClick(WaitUntilElement("//*[@id='header-search-form']"));
+
+		jsClick(WaitUntilElement("//input[@placeholder = 'What are you looking for today?...']"));
+
 		WaitUntilElement("//input[@class='jsx-1695452889 body2 pml-input__input']").sendKeys("56319");
 		WaitUntilElement("//input[@class='jsx-1695452889 body2 pml-input__input']").sendKeys(Keys.ENTER);
 
 		// Product View
-		WaitUntilElement("//div[@class='product-image__hover']//img[@alt='Short Sleeve Mandarin Collar Dress - Pink']")
-				.click();
+		jsClick(WaitUntilElement(
+				"//div[@class='product-image__hover']//img[@alt='Short Sleeve Mandarin Collar Dress - Pink']"));
 
 		WaitUntilElement("//button[.='L']").click();
 		WaitUntilElement("//span[.='Add To Bag']").click();
